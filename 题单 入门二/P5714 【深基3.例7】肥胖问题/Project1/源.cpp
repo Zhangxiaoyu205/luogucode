6e7aa65450d06513bddfdd12101bf16d77aec783
张晓宇 <https://github.com/Zhangxiaoyu205/luogu.git>
2026-01-12 14:57:01 +0800

    .

---
+++ 题单 入门二/P5714 【深基3.例7】肥胖问题/Project1/源.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	double m, h;
+	cin >> m >> h;
+	double BMI=m/(h*h);
+	if(BMI<18.5)
+	{
+		cout << "Underweight";
+	}
+	if(BMI>=18.5&&BMI<24)
+	{
+		cout << "Normal";
+	}
+	if(BMI>=24)
+	{
+		cout << setprecision(4) << fixed << BMI<<endl;
+		cout << "Overweight" << endl;
+	}
+}
