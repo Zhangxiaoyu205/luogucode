744d95dd3ad0fff49f7c8062547f1d2e2b2b62a9
张晓宇 <2556604708@qq.com>
2026-01-21 22:34:31 +0800

    5721

---
+++ 题单 入门三/P5721 【深基4.例6】数字直角三角形/Project1/源.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int x = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n+1 - i; j++)
+		{
+			if (x < 10)
+			{
+				cout << '0' << x;
+			}
+			else
+				cout << x;
+			x++;
+		}
+		cout << endl;
+	}
+}
